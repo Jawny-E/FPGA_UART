8de32d158b4757aa1d430949e40621c2bb96c6a6
Ellen Johnsen <94108006+Jawny-E@users.noreply.github.com>
2023-10-31 17:43:25 +0100

    Update main.vhd
    Exchanged all signal counters for variables

--- MILE-1/main.vhd
+++ MILE-1/main.vhd
@@ -40,10 +40,8 @@ architecture rtl of rx_module is
 	signal move_to_next  : std_logic := '0';							--Brukt til å dobbeltsjekke startbit
 --Klokkegenerering
 	constant clk_in_bit   : integer := (F_CLK/(BAUDRATE*SAMPLING*2)); --Talet på klokkepulsar frå 50Hz til rx_clk(8*baudrate)
-	signal rx_clk_counter : integer range 0 to CLK_IN_BIT := 0;		   --Teljar brukt til å generere rx_clk
 	signal rx_clk         : std_logic := '0';									--Signalet rx_clk
 --Dataavlesning
-	signal period_counter : integer range 0 to SAMPLING-1;								--Kontrollerar kvar vi samplar signalet
 	signal DATA_INDEX : integer range 0 to DATABITS-1 := 0;								--Kontrollerar kva databit som blir lest
 	signal rx_byte		: std_logic_vector(DATABITS-1 downto 0) := (others => '0'); --Mellombels lagring for innkommande data
 	signal rx_tests 	: std_logic_vector(4 downto 0) := (others => '0');				--Mellombels lagring av signalsampler
@@ -143,13 +141,14 @@ architecture rtl of rx_module is
 	-- bit. Dette gjerast vha. teljar
 	-----
 	clk_generator : process(clk)
-		begin
+	variable rx_clk_counter : integer range 0 to CLK_IN_BIT := 0;		   --Teljar brukt til å generere rx_clk
+	begin
 		if rising_edge(clk) then
 			if(rx_clk_counter = clk_in_bit) then
 				rx_clk <= not rx_clk;
-				rx_clk_counter <= 0;
+				rx_clk_counter := 0;
 			else
-				rx_clk_counter <= rx_clk_counter + 1;
+				rx_clk_counter := rx_clk_counter + 1;
 			end if;
 			--test_clk <= rx_clk;
 		end if;
@@ -162,12 +161,13 @@ architecture rtl of rx_module is
 	-- timing for systemet
 	-----
 	data_read : process(rx_clk)
+	variable period_counter : integer range 0 to SAMPLING-1;								--Kontrollerar kvar vi samplar signalet
 		begin
 		if rising_edge(rx_clk)then
 			case current_state is
 				when IDLE =>
 					DATA_INDEX <= 0;
-					period_counter <= 0;
+					period_counter := 0;
 					rx_byte <= (others => '0');
 					if rx_input = '0' then
 						current_state <= START_BIT;
@@ -179,21 +179,21 @@ architecture rtl of rx_module is
 					if period_counter = SAMPLING-1 then
 						if move_to_next = '1' then
 							current_state <= DATA_BIT;
-							period_counter <= 2; 		--Setter til 2 for å gjøre opp for timing-delays i denne casen
+							period_counter := 2; 		--Setter til 2 for å gjøre opp for timing-delays i denne casen
 						else
 							current_state <= IDLE;
 						end if;
 					elsif period_counter = SAMPLING/2 then
 						if rx_input = '0' then
 							move_to_next <= '1';
-							period_counter <= period_counter + 1;
+							period_counter := period_counter + 1;
 							current_state <= START_BIT;
 						else
 							current_state <= IDLE;
-							period_counter <= 0;
+							period_counter := 0;
 						end if;
 					else
-						period_counter <= period_counter + 1;
+						period_counter := period_counter + 1;
 					end if;
 					
 				when DATA_BIT =>
@@ -205,10 +205,10 @@ architecture rtl of rx_module is
 						--else
 							--test_sampling <= '0'; 
 						end if;
-						period_counter <= period_counter + 1;
+						period_counter := period_counter + 1;
 						current_state <= DATA_BIT;
 					else
-						period_counter <= 0;
+						period_counter := 0;
 						rx_byte(DATA_INDEX) <= majority_decision(rx_tests);
 						rx_tests <= "00000";
 						if DATA_INDEX < DATABITS-1 then
@@ -226,9 +226,9 @@ architecture rtl of rx_module is
 					
 				when PARITY_BIT =>
 					if period_counter < SAMPLING-1 then
-						period_counter <= period_counter + 1;
+						period_counter := period_counter + 1;
 					else
-						period_counter <= 0;
+						period_counter := 0;
 						if even_or_odd(rx_byte & rx_input) = PARITET_OP then
 							current_state <= STOP_BIT;
 						else
@@ -238,15 +238,15 @@ architecture rtl of rx_module is
 					
 				when STOP_BIT =>
 					if period_counter < SAMPLING/2 then
-						period_counter <= period_counter + 1;
+						period_counter := period_counter + 1;
 						current_state <= STOP_BIT;
 					else
-						period_counter <= 0;
+						period_counter := 0;
 						
 						if rx_input = '1' then
 							recived_flag <= '1';
 							recived_byte <= rx_byte;
-							period_counter <= 0;
+							period_counter := 0;
 							current_state <= PAUSE;
 						else
 							current_state <= IDLE;
@@ -256,9 +256,9 @@ architecture rtl of rx_module is
 					
 				when PAUSE =>
 					if period_counter < SAMPLING-1 then
-						period_counter <= period_counter + 1;
+						period_counter := period_counter + 1;
 					else
-						period_counter <= 0;
+						period_counter := 0;
 						current_state <= IDLE;
 						recived_flag <= '0';
 					end if;
